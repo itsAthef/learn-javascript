95119d9a37540925c520229df8de9650c5a3a97b
itsAthef <athefayubkhan2314@conestogac.on.ca>
2024-07-06 00:16:49 -0400

    print odd number

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // for loop
 
-for (let i=1; i<10; i++){
+// for (let i=1; i<10; i++){
+//     console.log(i)
+// }
+
+for (let i=1; i<=15; i=i+2){
     console.log(i)
+}
+
+console.log("backwards")
+
+for (let j=15; j>=1; j=j-2){
+    console.log(j+i)
 }
