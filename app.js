fe61bc07a35aaca8577af94b1a0baee517522230
itsAthef <athefayubkhan2314@conestogac.on.ca>
2024-07-06 00:21:32 -0400

    print even number

--- app.js
+++ app.js
@@ -4,12 +4,18 @@
 //     console.log(i)
 // }
 
-for (let i=1; i<=15; i=i+2){
+// for (let i=1; i<=15; i=i+2){
+//     console.log(i)
+// }
+
+// console.log("backwards")
+
+// for (let j=15; j>=1; j=j-2){
+//     console.log(j+i)
+// }
+
+// print even number
+
+for (let i=2; i<=20; i=i+2){
     console.log(i)
-}
-
-console.log("backwards")
-
-for (let j=15; j>=1; j=j-2){
-    console.log(j+i)
 }
